afabeacdd38ce9fb538a4ff686900cb2e8387895
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-01-02 16:14:03 -0300

    Create ex001-insert-users.sql

---
+++ sql/ex001-insert-users.sql
@@ -0,0 +1,5 @@
+insert into users (fist_name, last_name, email, password_hash, salary) values
+('Lucas', 'Mentis', 'lmct.temous@gmlms.con', 'KDDEETT', '8033332'),
+('Ana', 'Mantinele', 'anmlt.ssd@mail.coc', 'KKDDD3JK', '9929991'),
+('Maria', 'Santos', 'mass.sds@dsmd.net', 'HANNNDH', '9222292'), 
+('Sandro', 'Lucio', 'salu.ksk@dmsm.com', 'WTBDBAS', '2323232');
