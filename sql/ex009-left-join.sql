fc20d5fbb77a60db0d15cec852f17a8235b108ad
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-01-05 00:03:27 -0300

    Create ex009-left-join.sql

---
+++ sql/ex009-left-join.sql
@@ -0,0 +1,4 @@
+select u.id as uid, u.fist_name, r.name, p.bio from users u 
+left join user_roles ur on u.id = user_id 
+left join roles r on ur.roles_id = r.id 
+left join profiles p on p.user_id = u.id;
